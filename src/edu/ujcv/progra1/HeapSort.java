249ec5c59316c24c5cc78ba53d03a32f9adc7fc7
Jeymerd Canales <jeymerdvanales98@gmail.com>
2020-03-14 23:38:41 -0600

    Probando implementacion de PriorityBlockingQueue en HeapSort

--- src/edu/ujcv/progra1/HeapSort.java
+++ src/edu/ujcv/progra1/HeapSort.java
@@ -1,29 +1,38 @@
 package edu.ujcv.progra1;
 
-import java.util.PriorityQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class HeapSort implements SortTester {
     @Override
     public long sort(int[] array) {
         long start = System.currentTimeMillis();
-        heapSort(array);
+        new heapSort(array);
 
         long end = System.currentTimeMillis();
 
         return end - start;
     }
 
-    public static int[] heapSort(int[] array) {
-
-        // cambiar priority queue por su propia implementacion!!
-        PriorityQueue <Integer> queue = new PriorityQueue<>();
-        for (Integer i: array) {
-            queue.add(i);
-        }
-        for (int i = 0; i < array.length; i++) {
-            array[i] = queue.remove();
-        }
-        return array;
+    public HeapSort(int[] elementos) {
     }
 
-}
+    public static class heapSort {
+        public heapSort(int[] array) {
+        }
+
+        public static int[] heapSort(int[] array) {
+
+            PriorityBlockingQueue<Integer> pbq = new PriorityBlockingQueue<Integer>();
+
+            pbq.add(1);
+            pbq.add(2);
+            pbq.add(3);
+            pbq.add(4);
+            pbq.add(5);
+
+            System.out.println("PriorityBlockingQueue:"
+                    + pbq);
+            return array;
+        }
+    }
+    }
--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -6,14 +6,14 @@ public class Main {
 	// write your code here
 
         int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
-        MergeSort m = new MergeSort();
-        elementos = m.MergeSort(elementos);
+        HeapSort b = new HeapSort(elementos);
+        elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
 
         for (int i = 0; i < elementos.length; i++) {
             System.out.println(elementos[i]);
         }
 
-        elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
-        System.out.println("\n\ntarda "+ m.sort(elementos)+ " milisegundos");
+       // elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
+        System.out.println("\n\ntarda "+ b.sort(elementos)+ " milisegundos");
     }
 }
