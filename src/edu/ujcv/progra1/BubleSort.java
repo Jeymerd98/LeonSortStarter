bf68b0c8e42bb6b246318d8bcba83ddc9b6bfc28
Jeymerd Canales <jeymerdvanales98@gmail.com>
2020-03-14 22:47:44 -0600

    implementacion de BubbleSort

--- src/edu/ujcv/progra1/BubleSort.java
+++ src/edu/ujcv/progra1/BubleSort.java
@@ -4,24 +4,26 @@ public class BubleSort implements SortTester {
     @Override
     public long sort(int[] array) {
         long start = System.currentTimeMillis();
-        bubleSort(array);
+        BubleSort(array);
 
         long end = System.currentTimeMillis();
 
         return end - start;
     }
 
-    public static int[] bubleSort(int[] a){
-        for (int i = 0; i < a.length ; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if(a[i] > a[j]){
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+    public int[] BubleSort(int[] arr){
+
+        int temp;
+        for (int i = 0; i < arr.length ; i++){
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] > arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-        return a;
+        return arr;
     }
 }
 
