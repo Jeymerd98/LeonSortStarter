01e1e607e9e934ae3bc937c8349386042e3b8c8e
Jeymerd Canales <jeymerdvanales98@gmail.com>
2020-03-16 11:08:20 -0600

    Cambio de ejecucion de tiempo de MergeSort a nanoTime

--- src/edu/ujcv/progra1/HeapSort.java
+++ src/edu/ujcv/progra1/HeapSort.java
@@ -3,35 +3,29 @@ package edu.ujcv.progra1;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class HeapSort implements SortTester {
+
+
     @Override
     public long sort(int[] array) {
         long start = System.nanoTime();
-        new heapSort(array);
+        HeapSort(array);
 
         long end = System.nanoTime();
 
         return end - start;
     }
 
-    public HeapSort(int[] elementos) {
-    }
-
-    public static class heapSort {
-        public heapSort(int[] array) {
-        }
-
-        public static int[] heapSort(int[] array) {
+        public  int[] HeapSort(int[] array) {
 
             PriorityBlockingQueue<Integer> pbq = new PriorityBlockingQueue<Integer>();
 
             pbq.add(1);
             pbq.add(2);
+            pbq.add(5);
             pbq.add(3);
             pbq.add(4);
-            pbq.add(5);
 
             System.out.println("PriorityBlockingQueue:" + pbq);
             return array;
         }
-    }
     }
--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -6,8 +6,11 @@ public class Main {
 	// write your code here
 
         int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
-        MergeSort b = new MergeSort(elementos);
+        HeapSort b = new HeapSort();
+        elementos = b.HeapSort(elementos);
+
         elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
+
 
         for (int i = 0; i < elementos.length; i++) {
             System.out.println(elementos[i]);
